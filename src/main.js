5a802b6bbd95b9272b7e47f3d6c5594eedf3a5f5
TomRight <zxvc@rambler.ru>
2023-10-25 17:14:19 +0500

    Подключение vuex

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "@/router/router.js";
+import store from "@/store";
 
 const app = createApp(App);
 
-app.use(router).mount("#app");
+app.use(router).use(store).mount("#app");
