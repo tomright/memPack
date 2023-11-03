50a3f00dc28ac8d9e46e78d5c8e9a64308f60f3e
TomRight <zxvc@rambler.ru>
2023-11-03 17:45:12 +0500

    Создание базового селектора

---
+++ src/components/UI/BaseSelector.vue
@@ -0,0 +1,16 @@
+<template>
+  <select>
+    <option selected disabled>Сортировать по:</option>
+    <option>Лайкам</option>
+    <option>Просмотрам</option>
+    <option>Дате добавления</option>
+  </select>
+</template>
+
+<script>
+export default {
+  name: "BaseSector",
+};
+</script>
+
+<style></style>
