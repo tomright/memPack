94c9a59c3ac2d89cf5a9226bf1340bcadfd57af3
TomRight <zxvc@rambler.ru>
2023-11-03 17:45:00 +0500

    Создание сетки и дизайна каждого элемента мема

--- src/components/MemList.vue
+++ src/components/MemList.vue
@@ -1,5 +1,16 @@
 <template>
-  <div class="memList"></div>
+  //TODO передалть в компонент дизайн мемов
+  <div class="memList">
+    <div v-for="item in 30">
+      <div class="memList__memes">
+        <div class="memList__img"></div>
+        <div class="memList__control">
+          <div class="memList__like"></div>
+          <div class="memList__share"></div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -8,4 +19,48 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+.memList {
+  display: flex;
+  flex-wrap: wrap;
+  gap: 30px;
+  width: 100%;
+  padding: 10px;
+  justify-content: center;
+  align-items: center;
+}
+.memList__memes {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+  column-gap: 5px;
+
+  width: 210px;
+  height: 250px;
+  border: 2px solid green;
+}
+.memList__img {
+  background-image: url("@/assets/Img/AssCraving.jpg");
+  background-size: cover;
+  width: 200px;
+  height: 200px;
+}
+.memList__like {
+  background-image: url("@/assets/Img/noLike.svg");
+  background-size: contain;
+  width: 30px;
+  height: 30px;
+}
+.memList__share {
+  background-image: url("@/assets/Img/share.svg");
+  background-size: contain;
+  width: 30px;
+  height: 30px;
+}
+.memList__control {
+  display: flex;
+  justify-content: space-around;
+  width: 100%;
+}
+</style>
