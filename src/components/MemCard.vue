a7bf0d4482eafbddf5340d44b0148f978a152b40
TomRight <zxvc@rambler.ru>
2023-11-06 13:55:31 +0500

    Вывел карточку мема в отдельный компонент

---
+++ src/components/MemCard.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="memList__memes">
+    <div class="memList__img"></div>
+    <div class="memList__control">
+      <div class="memList__like"></div>
+      <div class="memList__share"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MemCard",
+};
+</script>
+
+<style>
+.memList__memes {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+  column-gap: 5px;
+
+  width: 210px;
+  height: 250px;
+  border: 2px solid green;
+}
+.memList__img {
+  background-image: url("@/assets/Img/AssCraving.jpg");
+  background-size: cover;
+  width: 200px;
+  height: 200px;
+}
+.memList__like {
+  background-image: url("@/assets/Img/noLike.svg");
+  background-size: contain;
+  width: 30px;
+  height: 30px;
+}
+.memList__share {
+  background-image: url("@/assets/Img/share.svg");
+  background-size: contain;
+  width: 30px;
+  height: 30px;
+}
+.memList__control {
+  display: flex;
+  justify-content: space-around;
+  width: 100%;
+}
+</style>
--- src/components/MemList.vue
+++ src/components/MemList.vue
@@ -2,24 +2,21 @@
   //TODO передалть в компонент дизайн мемов
   <div class="memList">
     <div v-for="item in 30">
-      <div class="memList__memes">
-        <div class="memList__img"></div>
-        <div class="memList__control">
-          <div class="memList__like"></div>
-          <div class="memList__share"></div>
-        </div>
-      </div>
+      <MemCard />
     </div>
   </div>
 </template>
 
 <script>
+import MemCard from "@/components/MemCard.vue";
+
 export default {
   name: "MemList",
+  components: { MemCard },
 };
 </script>
 
-<style>
+<style scoped>
 .memList {
   display: flex;
   flex-wrap: wrap;
@@ -28,39 +25,5 @@ export default {
   padding: 10px;
   justify-content: center;
   align-items: center;
-}
-.memList__memes {
-  display: flex;
-  flex-direction: column;
-  justify-content: space-around;
-  align-items: center;
-  column-gap: 5px;
-
-  width: 210px;
-  height: 250px;
-  border: 2px solid green;
-}
-.memList__img {
-  background-image: url("@/assets/Img/AssCraving.jpg");
-  background-size: cover;
-  width: 200px;
-  height: 200px;
-}
-.memList__like {
-  background-image: url("@/assets/Img/noLike.svg");
-  background-size: contain;
-  width: 30px;
-  height: 30px;
-}
-.memList__share {
-  background-image: url("@/assets/Img/share.svg");
-  background-size: contain;
-  width: 30px;
-  height: 30px;
-}
-.memList__control {
-  display: flex;
-  justify-content: space-around;
-  width: 100%;
 }
 </style>
