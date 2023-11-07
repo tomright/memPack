ab472693466eb66360be5ce925dec9ec0148f072
TomRight <zxvc@rambler.ru>
2023-11-07 21:54:26 +0500

    Добавление секции с тегами для мемов

--- src/components/MemCard.vue
+++ src/components/MemCard.vue
@@ -1,11 +1,24 @@
 <template>
   <div class="memList__memes">
     <div class="memList__img">
-      <img class="memList__memes-source" src="@/assets/Img/testimg1.jpg" alt="" />
+      <img class="memList__memes-source" src="@/assets/Img/testimg2.jpg" alt="" />
     </div>
     <div class="memList__control">
-      <div class="memList__like"></div>
-      <div class="memList__share"></div>
+      <div class="memList__social">
+        <div class="memList__like"></div>
+        <div class="memList__share"></div>
+      </div>
+      <div class="memList__tags">
+        <div class="memList__tag-effect">
+          <span>Пираты карибского моря</span>
+        </div> 
+        <div class="memList__tag-effect">
+          <span>Джек Воробей</span>
+        </div> 
+        <div class="memList__tag-effect">
+          <span>...</span>
+        </div> 
+      </div>
     </div>
   </div>
 </template>
@@ -25,8 +38,8 @@ export default {
   row-gap: 5px;
   padding: 5px;
 
-  width: 250px;
-  height: 210px;
+  width: 270px;
+  height: 250px;
   border: 2px solid green;
 }
 .memList__img {
@@ -38,6 +51,7 @@ export default {
 .memList__memes-source {
   height: 100%;
   width: 100%;
+  object-fit: cover;
 }
 .memList__like {
   background-image: url("@/assets/ui-img/noLike.svg");
@@ -53,7 +67,33 @@ export default {
 }
 .memList__control {
   display: flex;
-  justify-content: space-around;
+  justify-content: flex-start;
+  column-gap: 10px;
+  align-items: center;
   width: 100%;
+}
+.memList__social {
+  display: flex;
+  column-gap: 10px;
+}
+.memList__tags {
+  display: flex;
+  flex-wrap: wrap;
+  row-gap: 3px;
+  column-gap: 3px;
+  align-items: flex-start;
+  align-self: flex-start;
+  font-family: Helvetica, sans-serif;
+  font-size: 12px;
+  overflow: hidden;
+}
+.memList__tag-effect {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 20px;
+  padding: 7px;
+  background-color: rgba(128, 128, 128, 0.274);
+  border-radius: 10px;
 }
 </style>
