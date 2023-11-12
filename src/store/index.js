3d981a7e14ac0dbb2da63c48dfe09f7320a98a04
TomRight <zxvc@rambler.ru>
2023-11-12 23:22:51 +0500

    Добавление тестовых данных в стор

--- src/store/index.js
+++ src/store/index.js
@@ -2,6 +2,154 @@ import { createStore } from "vuex";
 
 export default createStore({
   state: {
-    test: "vuex is running",
+    memes: [
+      {
+        src: "./src/assets/Img/testimg.jpg",
+        like: false,
+        tags: ["Джек Воробей", "Пираты карбиского моря", "Джонни Депп", "Смекаешь?"],
+      },
+      {
+        src: "./src/assets/Img/testimg1.jpg",
+        like: false,
+        tags: ["Джек Воробей", "Пираты карбиского моря", "Джонни Депп", "Смекаешь?"],
+      },
+      {
+        src: "./src/assets/Img/testimg2.jpg",
+        like: false,
+        tags: ["Джек Воробей", "Пираты карбиского моря", "Джонни Депп", "Смекаешь?"],
+      },
+      {
+        src: "./src/assets/Img/testimg.jpg",
+        like: false,
+        tags: ["Джек Воробей", "Пираты карбиского моря", "Джонни Депп", "Смекаешь?"],
+      },
+      {
+        src: "./src/assets/Img/testimg1.jpg",
+        like: false,
+        tags: ["Джек Воробей", "Пираты карбиского моря", "Джонни Депп", "Смекаешь?"],
+      },
+      {
+        src: "./src/assets/Img/testimg2.jpg",
+        like: false,
+        tags: ["Джек Воробей", "Пираты карбиского моря", "Джонни Депп", "Смекаешь?"],
+      },
+      {
+        src: "./src/assets/Img/testimg.jpg",
+        like: false,
+        tags: ["Джек Воробей", "Пираты карбиского моря", "Джонни Депп", "Смекаешь?"],
+      },
+      {
+        src: "./src/assets/Img/testimg1.jpg",
+        like: false,
+        tags: ["Джек Воробей", "Пираты карбиского моря", "Джонни Депп", "Смекаешь?"],
+      },
+      {
+        src: "./src/assets/Img/testimg2.jpg",
+        like: false,
+        tags: ["Джек Воробей", "Пираты карбиского моря", "Джонни Депп", "Смекаешь?"],
+      },
+      {
+        src: "./src/assets/Img/testimg.jpg",
+        like: false,
+        tags: ["Джек Воробей", "Пираты карбиского моря", "Джонни Депп", "Смекаешь?"],
+      },
+      {
+        src: "./src/assets/Img/testimg1.jpg",
+        like: false,
+        tags: ["Джек Воробей", "Пираты карбиского моря", "Джонни Депп", "Смекаешь?"],
+      },
+      {
+        src: "./src/assets/Img/testimg2.jpg",
+        like: false,
+        tags: ["Джек Воробей", "Пираты карбиского моря", "Джонни Депп", "Смекаешь?"],
+      },
+      {
+        src: "./src/assets/Img/testimg.jpg",
+        like: false,
+        tags: ["Джек Воробей", "Пираты карбиского моря", "Джонни Депп", "Смекаешь?"],
+      },
+      {
+        src: "./src/assets/Img/testimg1.jpg",
+        like: false,
+        tags: ["Джек Воробей", "Пираты карбиского моря", "Джонни Депп", "Смекаешь?"],
+      },
+      {
+        src: "./src/assets/Img/testimg2.jpg",
+        like: false,
+        tags: ["Джек Воробей", "Пираты карбиского моря", "Джонни Депп", "Смекаешь?"],
+      },
+      {
+        src: "./src/assets/Img/testimg.jpg",
+        like: false,
+        tags: ["Джек Воробей", "Пираты карбиского моря", "Джонни Депп", "Смекаешь?"],
+      },
+      {
+        src: "./src/assets/Img/testimg1.jpg",
+        like: false,
+        tags: ["Джек Воробей", "Пираты карбиского моря", "Джонни Депп", "Смекаешь?"],
+      },
+      {
+        src: "./src/assets/Img/testimg2.jpg",
+        like: false,
+        tags: ["Джек Воробей", "Пираты карбиского моря", "Джонни Депп", "Смекаешь?"],
+      },
+      {
+        src: "./src/assets/Img/testimg.jpg",
+        like: false,
+        tags: ["Джек Воробей", "Пираты карбиского моря", "Джонни Депп", "Смекаешь?"],
+      },
+      {
+        src: "./src/assets/Img/testimg1.jpg",
+        like: false,
+        tags: ["Джек Воробей", "Пираты карбиского моря", "Джонни Депп", "Смекаешь?"],
+      },
+      {
+        src: "./src/assets/Img/testimg2.jpg",
+        like: false,
+        tags: ["Джек Воробей", "Пираты карбиского моря", "Джонни Депп", "Смекаешь?"],
+      },
+      {
+        src: "./src/assets/Img/testimg.jpg",
+        like: false,
+        tags: ["Джек Воробей", "Пираты карбиского моря", "Джонни Депп", "Смекаешь?"],
+      },
+      {
+        src: "./src/assets/Img/testimg1.jpg",
+        like: false,
+        tags: ["Джек Воробей", "Пираты карбиского моря", "Джонни Депп", "Смекаешь?"],
+      },
+      {
+        src: "./src/assets/Img/testimg2.jpg",
+        like: false,
+        tags: ["Джек Воробей", "Пираты карбиского моря", "Джонни Депп", "Смекаешь?"],
+      },
+      {
+        src: "./src/assets/Img/testimg.jpg",
+        like: false,
+        tags: ["Джек Воробей", "Пираты карбиского моря", "Джонни Депп", "Смекаешь?"],
+      },
+      {
+        src: "./src/assets/Img/testimg1.jpg",
+        like: false,
+        tags: ["Джек Воробей", "Пираты карбиского моря", "Джонни Депп", "Смекаешь?"],
+      },
+      {
+        src: "./src/assets/Img/testimg2.jpg",
+        like: false,
+        tags: ["Джек Воробей", "Пираты карбиского моря", "Джонни Депп", "Смекаешь?"],
+      },
+    ],
+  },
+  getters: {
+    //TODO Сделать сортировку и поиск мемов
+  },
+  mutations: {
+    likeToggle(state, payload) {
+      if (state.payload) {
+        state.payload = false;
+      } else {
+        state.payload = true;
+      }
+    },
   },
 });
