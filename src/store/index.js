274117fe1c39e91b1a5bb68d56d1938b7e27ddbc
TomRight <zxvc@rambler.ru>
2023-11-16 17:33:58 +0500

    Дополнение тестовых данных

--- src/store/index.js
+++ src/store/index.js
@@ -4,211 +4,253 @@ export default createStore({
   state: {
     memes: [
       {
+        id: 0,
         src: "./src/assets/Img/testimg0.jpg",
         like: false,
         tags: ["Дэвид Духовны ", "Секретные материалы", "Фокс Малдер"],
       },
       {
+        id: 1,
         src: "./src/assets/Img/testimg1.jpg",
         like: false,
         tags: ["Кот", "хмурый кот"],
       },
       {
+        id: 2,
         src: "./src/assets/Img/testimg2.jpg",
         like: false,
         tags: ["Дикаприо", "Картинка с текстом", "Леонардо Дикаприо"],
       },
       {
+        id: 3,
         src: "./src/assets/Img/testimg3.jpg",
         like: false,
         tags: ["Дикаприо", "Картинка с текстом", "Леонардо Дикаприо"],
       },
       {
+        id: 4,
         src: "./src/assets/Img/testimg4.jpg",
         like: false,
         tags: ["Дикаприо", "Картинка с текстом", "Леонардо Дикаприо"],
       },
       {
+        id: 5,
         src: "./src/assets/Img/testimg5.jpg",
         like: false,
         tags: ["Дикаприо", "Картинка с текстом", "Леонардо Дикаприо"],
       },
       {
+        id: 6,
         src: "./src/assets/Img/testimg6.webp",
         like: false,
         tags: ["Дикаприо", "Картинка с текстом", "Леонардо Дикаприо"],
       },
       {
+        id: 7,
         src: "./src/assets/Img/testimg7.jpg",
         like: false,
         tags: ["Дикаприо", "Картинка с текстом", "Леонардо Дикаприо"],
       },
       {
+        id: 8,
         src: "./src/assets/Img/testimg8.jpg",
         like: false,
         tags: ["Дикаприо", "Картинка с текстом", "Леонардо Дикаприо"],
       },
       {
+        id: 9,
         src: "./src/assets/Img/testimg9.jpg",
         like: false,
         tags: ["Дикаприо", "Картинка с текстом", "Леонардо Дикаприо"],
       },
       {
+        id: 10,
         src: "./src/assets/Img/testimg10.jpg",
         like: false,
         tags: ["Дикаприо", "Картинка с текстом", "Леонардо Дикаприо"],
       },
       {
+        id: 11,
         src: "./src/assets/Img/testimg11.jpg",
         like: false,
         tags: ["Дикаприо", "Картинка с текстом", "Леонардо Дикаприо"],
       },
       {
+        id: 12,
         src: "./src/assets/Img/testimg12.jpg",
         like: false,
         tags: ["Дикаприо", "Картинка с текстом", "Леонардо Дикаприо"],
       },
       {
+        id: 13,
         src: "./src/assets/Img/testimg13.jpg",
         like: false,
         tags: ["Дикаприо", "Картинка с текстом", "Леонардо Дикаприо"],
       },
       {
+        id: 14,
         src: "./src/assets/Img/testimg0.jpg",
         like: false,
         tags: ["Дэвид Духовны ", "Секретные материалы", "Фокс Малдер"],
       },
       {
+        id: 15,
         src: "./src/assets/Img/testimg1.jpg",
         like: false,
         tags: ["Кот", "хмурый кот"],
       },
       {
+        id: 16,
         src: "./src/assets/Img/testimg2.jpg",
         like: false,
         tags: ["Дикаприо", "Картинка с текстом", "Леонардо Дикаприо"],
       },
       {
+        id: 17,
         src: "./src/assets/Img/testimg3.jpg",
         like: false,
         tags: ["Дикаприо", "Картинка с текстом", "Леонардо Дикаприо"],
       },
       {
+        id: 18,
         src: "./src/assets/Img/testimg4.jpg",
         like: false,
         tags: ["Дикаприо", "Картинка с текстом", "Леонардо Дикаприо"],
       },
       {
+        id: 19,
         src: "./src/assets/Img/testimg8.jpg",
         like: false,
         tags: ["Дикаприо", "Картинка с текстом", "Леонардо Дикаприо"],
       },
       {
+        id: 20,
         src: "./src/assets/Img/testimg5.jpg",
         like: false,
         tags: ["Дикаприо", "Картинка с текстом", "Леонардо Дикаприо"],
       },
       {
+        id: 21,
         src: "./src/assets/Img/testimg6.webp",
         like: false,
         tags: ["Дикаприо", "Картинка с текстом", "Леонардо Дикаприо"],
       },
       {
+        id: 22,
         src: "./src/assets/Img/testimg7.jpg",
         like: false,
         tags: ["Дикаприо", "Картинка с текстом", "Леонардо Дикаприо"],
       },
       {
+        id: 23,
         src: "./src/assets/Img/testimg9.jpg",
         like: false,
         tags: ["Дикаприо", "Картинка с текстом", "Леонардо Дикаприо"],
       },
       {
+        id: 24,
         src: "./src/assets/Img/testimg10.jpg",
         like: false,
         tags: ["Дикаприо", "Картинка с текстом", "Леонардо Дикаприо"],
       },
       {
+        id: 25,
         src: "./src/assets/Img/testimg11.jpg",
         like: false,
         tags: ["Дикаприо", "Картинка с текстом", "Леонардо Дикаприо"],
       },
       {
+        id: 26,
         src: "./src/assets/Img/testimg12.jpg",
         like: false,
         tags: ["Дикаприо", "Картинка с текстом", "Леонардо Дикаприо"],
       },
       {
+        id: 27,
         src: "./src/assets/Img/testimg13.jpg",
         like: false,
         tags: ["Дикаприо", "Картинка с текстом", "Леонардо Дикаприо"],
       },
       {
+        id: 28,
         src: "./src/assets/Img/testimg0.jpg",
         like: false,
         tags: ["Дэвид Духовны ", "Секретные материалы", "Фокс Малдер"],
       },
       {
+        id: 29,
         src: "./src/assets/Img/testimg1.jpg",
         like: false,
         tags: ["Кот", "хмурый кот"],
       },
       {
+        id: 30,
         src: "./src/assets/Img/testimg2.jpg",
         like: false,
         tags: ["Дикаприо", "Картинка с текстом", "Леонардо Дикаприо"],
       },
       {
+        id: 31,
         src: "./src/assets/Img/testimg3.jpg",
         like: false,
         tags: ["Дикаприо", "Картинка с текстом", "Леонардо Дикаприо"],
       },
       {
+        id: 32,
         src: "./src/assets/Img/testimg4.jpg",
         like: false,
         tags: ["Дикаприо", "Картинка с текстом", "Леонардо Дикаприо"],
       },
       {
+        id: 33,
         src: "./src/assets/Img/testimg5.jpg",
         like: false,
         tags: ["Дикаприо", "Картинка с текстом", "Леонардо Дикаприо"],
       },
       {
+        id: 34,
         src: "./src/assets/Img/testimg6.webp",
         like: false,
         tags: ["Дикаприо", "Картинка с текстом", "Леонардо Дикаприо"],
       },
       {
+        id: 35,
         src: "./src/assets/Img/testimg7.jpg",
         like: false,
         tags: ["Дикаприо", "Картинка с текстом", "Леонардо Дикаприо"],
       },
       {
+        id: 36,
         src: "./src/assets/Img/testimg9.jpg",
         like: false,
         tags: ["Дикаприо", "Картинка с текстом", "Леонардо Дикаприо"],
       },
       {
+        id: 37,
         src: "./src/assets/Img/testimg10.jpg",
         like: false,
         tags: ["Дикаприо", "Картинка с текстом", "Леонардо Дикаприо"],
       },
       {
+        id: 38,
         src: "./src/assets/Img/testimg11.jpg",
         like: false,
         tags: ["Дикаприо", "Картинка с текстом", "Леонардо Дикаприо"],
       },
       {
+        id: 39,
         src: "./src/assets/Img/testimg8.jpg",
         like: false,
         tags: ["Дикаприо", "Картинка с текстом", "Леонардо Дикаприо"],
       },
       {
+        id: 40,
         src: "./src/assets/Img/testimg12.jpg",
         like: false,
         tags: ["Дикаприо", "Картинка с текстом", "Леонардо Дикаприо"],
       },
       {
+        id: 41,
         src: "./src/assets/Img/testimg13.jpg",
         like: false,
         tags: ["Дикаприо", "Картинка с текстом", "Леонардо Дикаприо"],
@@ -220,11 +262,14 @@ export default createStore({
   },
   mutations: {
     likeToggle(state, payload) {
-      if (state.payload) {
-        state.payload = false;
-      } else {
-        state.payload = true;
-      }
+      state.memes[payload].like = !state.memes[payload].like;
+    },
+  },
+  actions: {
+    likeImg({ commit }, payload) {
+      // payload {id}
+      commit("likeToggle", payload);
+      //TODO отправка на сервер
     },
   },
 });
