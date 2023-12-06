39dd3f493b81372d6bb076ac7dd57ea6e9b03a8a
TomRight <zxvc@rambler.ru>
2023-12-06 21:59:36 +0500

    Добавил элементы в форму входа

--- src/pages/logInPage.vue
+++ src/pages/logInPage.vue
@@ -1,7 +1,36 @@
 <template>
-  <h1>Вход</h1>
+  <div class="login">
+    <h1>Вход</h1>
+    <form method="post" @submit.prevent="login" class="login__form">
+      <label to="login">Логин</label>
+      <BaseInput id="login"></BaseInput>
+      <label to="pass">Логин</label>
+      <BaseInput id="pass"></BaseInput>
+      <BaseButton class="login__entBtn">Вход</BaseButton>
+    </form>
+  </div>
 </template>
 
-<script></script>
+<script>
+import BaseButton from "../components/UI/BaseButton.vue";
+import BaseInput from "../components/UI/BaseInput.vue";
+export default {
+  components: { BaseInput, BaseButton },
+};
+</script>
 
-<style></style>
+<style scoped>
+.login {
+  display: flex;
+  flex-direction: column;
+  row-gap: 15px;
+}
+.login__form {
+  display: flex;
+  flex-direction: column;
+  row-gap: 10px;
+}
+.login__entBtn {
+  margin-top: 20px;
+}
+</style>
