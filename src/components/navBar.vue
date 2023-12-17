1c3a799fb4312223f8365cd76f4f33de15c3e526
TomRight <zxvc@rambler.ru>
2023-12-17 22:07:36 +0500

    часть мобильного меню

--- src/components/navBar.vue
+++ src/components/navBar.vue
@@ -1,11 +1,16 @@
 <template>
-  <div class="navBar__line">
-    <BaseButton @click="$router.push('/')">Все мемы</BaseButton>
-    <BaseButton @click="$router.push('/favorit')">Ваши мемы</BaseButton>
-    <BaseButton @click="$router.push('/aboutus')">О приложении</BaseButton>
-    <BaseButton v-if="!$store.state.loggined" @click="$router.push('/register')">Регистрация</BaseButton>
-    <BaseButton v-if="!$store.state.loggined" @click="$router.push('/login')">Войти</BaseButton>
-    <BaseButton v-if="$store.state.loggined">Выход</BaseButton>
+  <div class="navbar__wrapper">
+    <div class="navbar__menu-burger">
+      <img class="navbar__burger-img" src="@/assets/ui-img/burger.svg" alt="Меню" />
+    </div>
+    <div class="navBar__line">
+      <BaseButton @click="$router.push('/')">Все мемы</BaseButton>
+      <BaseButton @click="$router.push('/favorit')">Ваши мемы</BaseButton>
+      <BaseButton @click="$router.push('/aboutus')">О приложении</BaseButton>
+      <BaseButton v-if="!$store.state.loggined" @click="$router.push('/register')">Регистрация</BaseButton>
+      <BaseButton v-if="!$store.state.loggined" @click="$router.push('/login')">Войти</BaseButton>
+      <BaseButton v-if="$store.state.loggined">Выход</BaseButton>
+    </div>
   </div>
 </template>
 
@@ -25,5 +30,29 @@ export default {
   display: flex;
   column-gap: 10px;
   justify-content: center;
+}
+.navbar__menu-burger {
+  display: none;
+}
+@media (max-width: 991.98px) {
+  .navbar__menu-burger {
+    z-index: 9999;
+    display: flex;
+    position: fixed;
+    top: 55%;
+    right: 0;
+    background-color: white;
+  }
+  .navbar__burger-img {
+  width: 35px;
+  }
+  .navBar__line {
+    position: fixed;
+    top: 60%;
+    right: 0px;
+    display: flex;
+    flex-direction: column;
+    row-gap: 10px;
+  }
 }
 </style>
